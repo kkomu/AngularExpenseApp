f790ddb4481355707f905506cd698dd668b0c752
Kyo Komu <ask@ifyoudare.com>
2015-05-03 19:42:06 +0300

    First version of form

--- app/controllers/viewOneController.js
+++ app/controllers/viewOneController.js
@@ -1,36 +1,86 @@
+// Main controller for New Expense Claim view
 expenseMod.controller("ViewOneController", function ($scope) {
     'use strict';
     
-    $scope.expenses = [];
-    /*
+    // Array to keep all different types in form
+    $scope.travelFinExpenses = [];
+    $scope.travelForExpenses = [];
+    $scope.otherExpenses = [];
+
+    // Array to be used with ng-repeat to view correct amount of form-elements
+    $scope.travelFinCounter = [1];
+    $scope.travelForCounter = [1];
+    
+    $scope.newTravelFinForm = function() {
+        console.log($scope.travelFinExpenses);
+        $scope.travelFinCounter.push(1);
+    };
+    
+    $scope.removeTravelFinForm = function() {
+        // Always show one instance of partial form
+        if($scope.travelFinCounter.length > 1) {
+            $scope.travelFinCounter.pop();
+            $scope.travelFinExpenses.pop();
+        }
+    };
+    
+     $scope.newTravelForForm = function() {
+        console.log($scope.travelForExpenses);
+        $scope.travelForCounter.push(1);
+    };
+    
+    $scope.removeTravelForForm = function() {
+        // Always show one instance of partial form
+        if($scope.travelForCounter.length > 1) {
+            $scope.travelForCounter.pop();
+            $scope.travelForExpenses.pop();
+        }
+    };
+    
+});
+
+// Controller for Travel Expense Finland
+expenseMod.controller("TravelExpenseFinController", function($scope) {
+    
     $scope.travelExpense = {
-        index: 0,
-        destination: 'Joensuu',
-        leaveDate: Date,
-        returnDate: Date,
+        index: $scope.travelFinExpenses.length,
+        destination: $scope.destination,
+        leaveDate: $scope.leaveDate,
+        returnDate: $scope.returnDate,
+        halfDay: $scope.halfDay,
+        leaveTime: $scope.leaveTime,
+        returnTime: $scope.returnTime,
+        fullDay: $scope.returnTime
     };
-    */
-    $scope.counter = 1;
+    $scope.travelFinExpenses.push($scope.travelExpense);
+});
+
+// Controller for Travel Expense Foreign
+expenseMod.controller("TravelExpenseForController", function($scope) {
     
-    $scope.change = function() {
-        $scope.counter++;
-        console.log($scope.counter);
-        
-        var expense = {
-            index: $scope.expenses.length,
-            destination: $scope.destinationText,
-            leaveDate: '04-08-2015',
-            returnDate: '06-09-2015'
-        };
-        
-        $scope.expenses.push(expense);
-        //console.log($scope);
-        console.log($scope.expenses);
+    $scope.travelExpense = {
+        index: $scope.travelForExpenses.length,
+        destination: $scope.destination,
+        leaveDate: $scope.leaveDate,
+        returnDate: $scope.returnDate,
+        halfDay: $scope.halfDay,
+        leaveTime: $scope.leaveTime,
+        returnTime: $scope.returnTime,
+        fullDay: $scope.returnTime
     };
+    $scope.travelForExpenses.push($scope.travelExpense);
+});
+
+// Controller for Travel Expense Finland
+expenseMod.controller("OtheExpenseController", function($scope) {
     
-    // 
-    $scope.getNumber = function(num) {
-        return new Array(num);
+    $scope.otherExpense = {
+            index: $scope.otherExpenses.length,
+            purchaseDate: $scope.purchaseDate,
+            description: $scope.description,
+            price: $scope.price,
+            amount: $scope.amount,
+            attachedFile: $scope.attachedFile
     };
-    
+    $scope.otherExpenses.push($scope.otherExpense);
 });
---
+++ app/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+// Include plugins
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	replaceString: /\bgulp[\-.]/
+});
+// Define default destination folder
+var dest = 'app/vendor/';
